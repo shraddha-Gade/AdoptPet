7ae202e396c9f81aa803c44611c9756f731d6b95
shraddha-Gade <shraddhagade43@gmail.com>
2024-03-22 16:32:46 +0530

    Update and Delete operations on pet

--- pet_rehome/urls.py
+++ pet_rehome/urls.py
@@ -13,4 +13,11 @@ urlpatterns = [
     path('search/', views.search_results, name='search_results'),
     path('cat_breeds/', views.available_cat_breeds, name='available_cat_breeds'),
     path('dog_breeds/', views.available_dog_breeds, name='available_dog_breeds'),
+    path('cats/<int:cat_id>/', views.cat_profile, name='cat_profile'),
+    path('dogs/<int:dog_id>/', views.dog_profile, name='dog_profile'),
+    path('update_cat_profile/<int:cat_id>/', views.update_cat_profile, name='update_cat_profile'),
+    path('delete_cat_profile/<int:cat_id>/', views.delete_cat_profile, name='delete_cat_profile'),
+    path('update_dog_profile/<int:dog_id>/', views.update_dog_profile, name='update_dog_profile'),
+    path('delete_dog_profile/<int:dog_id>/', views.delete_dog_profile, name='delete_dog_profile'),
+    
 ]
